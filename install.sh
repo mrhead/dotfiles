2f2a55888634169a6b7f5be1db57e02bd2759076
Patrik Bóna <patrik.bona@mrhead.sk>
2017-10-29 21:10:00 +0100

    Add Brewfile to ignored files list

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ for name in *; do
       echo "WARNING: $target exists but is not a symlink."
     fi
   else
-    if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ] && [ "$name" != 'LICENSE' ]; then
+    if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ] && [ "$name" != 'LICENSE' ] && [ "$name" != 'Brewfile' ]; then
       echo "Creating $target"
       ln -s "$PWD/$name" "$target"
     fi
